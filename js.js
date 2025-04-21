4747b74a42da83ad630c57dc7d3f8094c42ce521
Ruslan Amirkhanov <amirhanov.ruslan23@yandex.ru>
2025-04-21 23:44:06 +0500

    Js.js

--- js.js
+++ js.js
@@ -2,19 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('catModal');
     const btn = document.querySelector('.button-link');
 
-    // Открытие модального окна
     btn.addEventListener('click', () => {
         modal.style.display = 'flex';
     });
 
-    // Закрытие при клике вне гифки
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
             modal.style.display = 'none';
         }
     });
 
-    // Закрытие по Escape
     document.addEventListener('keydown', (e) => {
         if (e.key === 'Escape' && modal.style.display === 'flex') {
             modal.style.display = 'none';
